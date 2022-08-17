abe5e8c787f64a4cae3aa75fc9304e783d7ace50
Alle43221 <allemoroz14@gmail.com>
2022-08-17 22:34:09 +0300

    Addapted the Navbar to vue.js

---
+++ src/components/FooterView.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="footer">
+    <div class="social-links">
+      <a href="https://github.com/Perpetuum-Mobile-RO065" target="_blank" class="social">
+        <img class="icon git-icon" src="pictures/10.png" alt="Github social link"/>
+      </a>
+      <a href="https://www.instagram.com/perpetuummobileftc/" target="_blank" class="social">
+        <img class="icon insta-icon" src="pictures/12.png" alt="Instagram social link"/>
+      </a>
+      <a href="https://discord.gg/8CPhWQCHhY" target="_blank" class="social">
+        <img class="icon dis-icon" src="pictures/13.png" alt="Discord social link"/>
+      </a>
+      <a href="https://www.facebook.com/perpetuummobilefirsttech/" target="_blank" class="social">
+        <img class="icon -icon" src="pictures/15-1.png" alt="social link"/>
+      </a>
+    </div>
+    <p class="copyright">Copyright© 2022 Perpetuum Mobile. Toate drepturile rezervate. Made with 🖤 by Alex David and Alexandra Moroz.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterView.vue"
+}
+</script>
+
+<style scoped>
+.footer{
+  background: rgb(100, 164, 70);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.8em;
+  font-weight: bold;
+  color:rgb(33, 33, 33);
+  font-family: 'Trispace', sans-serif;
+
+  position:relative;
+  bottom:0;
+  height: 11.5vh;
+  width: 100%;
+  padding: 1.5vh 0 1.5vh 0;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  border-top: solid 0.6vh rgb(26, 105, 66);
+}
+
+.social-links{
+  display: inline-block;
+  width: 60rem;
+  text-align: center;
+}
+
+.icon{
+  cursor: pointer;
+}
+
+.icon:hover{
+  -webkit-filter: drop-shadow(0px 0px 5px  RGB(169, 204, 141));
+  filter: drop-shadow(0px 0px 5px RGB(169, 204, 141));
+}
+
+.insta-icon{
+  width: 4%;
+  margin:0;
+  padding: 0;
+}
+
+.dis-icon{
+  width: 5%;
+  margin:0;
+  padding: 0;
+}
+
+.git-icon{
+  width: 4%;
+  margin:0;
+  padding: 0;
+}
+
+.-icon{
+  width: 4%;
+  margin:0;
+  padding: 0;
+}
+
+.copyright{
+  margin: 0;
+  padding: 0;
+}
+
+.social{
+  padding: 0;
+  margin: 0 75px 0 75px;
+  color:transparent;
+}
+</style>
---
+++ src/components/NavbarView.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="navigation--links">
+    <div class="logo">
+      <img class="logo" src="assets/logo_1-removebg-preview.png" alt="Perpetuum Mobile Logo">
+    </div>
+    <div class="sections">
+      <nav class="navigation--link ">Acasă</nav>
+      <nav class="navigation--link">Echipa noastră</nav>
+      <nav class="navigation--link">Sponsori</nav>
+      <nav class="navigation--link">Alătură-te echipei!</nav>
+      <nav class="navigation--link active">Contact</nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavbarView.vue"
+}
+</script>
+
+<style scoped>
+.navigation--links{
+  display:flex;
+  flex-direction:row;
+  background: rgb(100, 164, 70);
+  font-size: 1.5em;
+  margin:0;
+  color:rgb(33, 33, 33);
+  border-bottom: solid 0.6vh rgb(26, 105, 66);
+  justify-content: space-between;
+  font-family: 'Trispace', sans-serif;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  align-items: center;
+  height: 8.8vh;
+}
+
+.sections {
+  display: flex;
+  justify-content: flex-end;
+  flex-direction:row;
+}
+
+.navigation--link{
+  font-weight: bold;
+  margin: 10px 30px 10px 10px;
+  cursor: pointer;
+}
+
+.navigation--link:hover {
+  -webkit-transition: text-shadow, color 200ms ease;
+  -moz-transition: text-shadow, color 200ms ease;
+  -o-transition: text-shadow, color 200ms ease;
+
+  color: RGB(169, 204, 141);
+  text-shadow: -1.5px 1.5px 0 rgb(26, 105, 66),
+  1.5px 1.5px 0 rgb(26, 105, 66),
+  1.5px -1.5px 0 rgb(26, 105, 66),
+  -1.5px -1.5px 0 rgb(26, 105, 66);
+}
+
+.logo{
+  width: 25%;
+  margin-left: 30px;
+  cursor: pointer;
+  -webkit-filter: drop-shadow(0px 0px 8px #222);
+  filter: drop-shadow(0px 0px 8px #222);
+}
+
+.active{
+  color: RGB(169, 204, 141);
+  text-shadow: -1.5px 1.5px 0 rgb(26, 105, 66),
+  1.5px 1.5px 0 rgb(26, 105, 66),
+  1.5px -1.5px 0 rgb(26, 105, 66),
+  -1.5px -1.5px 0 rgb(26, 105, 66);
+}
+</style>
